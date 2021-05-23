1309b40aca0baac196a78e10c4e2fd0d35a0f855
David Coats <david@getsafetycone.com>
2021-05-23 02:38:38 -0700

    Add R5T.L0010.Extensions library with extension methods to add Razor class library runtime compilation.

---
+++ source/R5T.L0010.Extensions/Code/Extensions/MvcRazorRuntimeCompilationOptionsExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class MvcRazorRuntimeCompilationOptionsExtensions
+    {
+        public static MvcRazorRuntimeCompilationOptions AddRazorClassLibraryRuntimeCompilation(this MvcRazorRuntimeCompilationOptions options, string libraryDirectoryPath)
+        {
+            var fileProvider = new PhysicalFileProvider(libraryDirectoryPath);
+
+            options.FileProviders.Add(fileProvider);
+
+            return options;
+        }
+
+        public static MvcRazorRuntimeCompilationOptions AddRazorClassLibraryRuntimeCompilation(this MvcRazorRuntimeCompilationOptions options, string hostContentRootPath, string libraryRelativeDirectoryPath)
+        {
+            var libraryDirectoryPath = Path.GetFullPath(Path.Combine(hostContentRootPath, libraryRelativeDirectoryPath));
+
+            return options.AddRazorClassLibraryRuntimeCompilation(libraryDirectoryPath);
+        }
+
+        public static MvcRazorRuntimeCompilationOptions AddRazorClassLibraryRuntimeCompilation(this MvcRazorRuntimeCompilationOptions options, IHostEnvironment hostEnvironment, string libraryRelativeDirectoryPath)
+        {
+            return options.AddRazorClassLibraryRuntimeCompilation(hostEnvironment.ContentRootPath, libraryRelativeDirectoryPath);
+        }
+    }
+}
